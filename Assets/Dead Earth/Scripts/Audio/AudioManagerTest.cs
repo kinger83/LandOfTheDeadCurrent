acd739e3ad9b89dff52a7710b18eb065e8cacadb
kinger83 <31791925+kinger83@users.noreply.github.com>
2020-08-29 21:57:11 +1000

    lesson 44 complete

--- Assets/Dead Earth/Scripts/Audio/AudioManagerTest.cs
+++ Assets/Dead Earth/Scripts/Audio/AudioManagerTest.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class AudioManagerTest : MonoBehaviour
 {
+
+    public AudioClip clip;
+    public AudioClip clipTwo;
+    public AudioClip clipThree;
     // Start is called before the first frame update
     void Start()
     {
         if (AudioManager.instance)
         {
-            AudioManager.instance.SetTrackVolume("Zombies", 10);
-            AudioManager.instance.SetTrackVolume("Player", 20, 15);
-            AudioManager.instance.SetTrackVolume("UI", -10,3);
+            InvokeRepeating("PlayTestOne", .8f, .8f);
+
+          
 
         }
     }
 
+    void PlayTestOne()
+    {
+        AudioManager.instance.PlayOneShotSound("Player", clip, transform.position, 0.5f, 0.0f, 128);
+    }
+
+    void PlayTestTwo()
+    {
+        AudioManager.instance.PlayOneShotSound("Zombies", clipTwo, transform.position, 0.5f, 0.0f, 100);
+    }
+
+
+    void PlayTestThree()
+    {
+        AudioManager.instance.PlayOneShotSound("Scene", clipThree, transform.position, 0.5f, 0.0f, 100);
+    }
 }
